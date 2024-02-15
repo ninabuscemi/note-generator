6e29d20326e50e2a77ee85849cb95c1782cd48c6
Nina B <ngbuscemi@gmail.com>
2024-02-15 17:53:22 -0500

    added server.js file

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -5,6 +5,7 @@ let saveNoteBtn;
 let newNoteBtn;
 let noteList;
 
+
 if (window.location.pathname === '/notes') {
   noteForm = document.querySelector('.note-form');
   noteTitle = document.querySelector('.note-title');
